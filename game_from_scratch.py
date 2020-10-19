49f8c039906e2249154b4a7c921a0c6fa68e8094
Gobind Puniani <gobindpuniani@gmail.com>
2020-10-18 20:01:43 -0700

    Added file for NASA font and function for updating display elements

--- game_from_scratch.py
+++ game_from_scratch.py
@@ -5,6 +5,9 @@ import time
 import random
 
 
+# Initialize the font
+pygame.font.init()
+
 # Set display
 WIDTH, HEIGHT = 750, 750
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -25,8 +28,8 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-# Background
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+# Background scaled to fit entire screen
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
 # Main function
@@ -34,13 +37,29 @@ def main():
     run = True
     # Frames per second
     FPS = 60
-
+    level = 1
+    lives = 5
+    # Font
+    main_font = pygame.font.Font(os.path.join("assets", "nasalization-rg.ttf"), 50)
     # Clock object
     clock = pygame.time.Clock()
+
+    # Below are functions defined here within main() since they use other variables defined in main()
+
+    def redraw_window():
+        WINDOW.blit(BG, (0,0))
+        # Draw text for level and lives labels and place them on the top corners
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 100, 0))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 100, 0))
+        WINDOW.blit(lives_label, (WIDTH - lives_label.get_width() - 20, 15))
+        WINDOW.blit(level_label, (20, 15))
+        pygame.display.update()
 
     while run:
         # Run game at consistent speed across all computers
         clock.tick(FPS)
+        # Update all of the display surfaces
+        redraw_window()
 
         # Quit game when user clicks exit button
         for event in pygame.event.get():
